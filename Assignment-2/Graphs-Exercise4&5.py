a74e5983e0d9e89c2045ae79322dd98618d7246f
keshon primus <90883812+keshonprimus@users.noreply.github.com>
2022-07-10 05:14:28 -0400

    Create Graphs-Exercise4&5.py

---
+++ Assignment-2/Graphs-Exercise4&5.py
@@ -0,0 +1,49 @@
+class Graph: #Graph Class
+    def __init__(self):
+        self.graph = {}
+        
+        
+    def addEdge(self, node1, node2): #Adds edge to graph structure
+        self.graph[node1] = []
+        self.graph[node1].append(node2)
+        
+    def BFS_Shortest_Path(self, graph, start, goal): #Shortest Path via BFS search
+        explored = []
+        
+        q = [[start]]
+        
+        if start == goal:
+            print("Same Node lol")
+            return
+        
+        while q:
+            path = q.pop(0)
+            node = path[-1]
+            
+            if node not in explored:
+                negihbours = self.graph[node]
+                
+                for negihbour in negihbours:
+                    new_path = list(path)
+                    new_path.append(negihbour)
+                    q.append(new_path)
+                    
+                    if negihbour == goal:
+                        print("Shortest path = ", new_path)
+                        return
+                explored.append(node)
+                
+        print("So sorry, but a connecting path doesn't exist :(")
+        return
+    
+    
+#Test Cases   
+g = Graph()
+g.addEdge(1, 5)
+g.addEdge(0, 5)
+g.addEdge(5, 2)
+g.addEdge(1, 4)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+g.BFS_Shortest_Path(g, 0, 2)
